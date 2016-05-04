9bcb36736f270ce6932e22b55caa82179baa899e
sunadkat <sonalunadkat101@gmail.com>
2016-05-04 05:08:47 +0000

    number minor update

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,7 +1,6 @@
 class Rsvp < ActiveRecord::Base
     validates :fname, :name, :email, :address, :number, :event, :attending, presence: true
     validates :number, length: {maximum:10}
-    validates :number, length: {minimum:10}
     validates :number, numericality: true
     validates :fname, :name, format: {with: /\A[a-zA-Z]+\z/ }
     
